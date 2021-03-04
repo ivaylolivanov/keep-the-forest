5b2c9ef2f147bfdd6c1989dcd56bdc8d381a960e
ivaylolivanov <ivaylolivanov95@gmail.com>
2021-03-04 17:40:17 +0200

    Attackers animation for attack and deal damage
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour {
+    [SerializeField] LayerMask targetLayer;
+    [SerializeField] float attackRange;
+    [SerializeField] int damage;
 
     float currentWalkSpeed;
     Animator animator;
+
+    Health targetHealth;
 
     void Start() {
         animator = GetComponent<Animator>();
@@ -13,9 +18,32 @@ public class Attacker : MonoBehaviour {
 
     void Update() {
         transform.Translate(Vector2.left * currentWalkSpeed * Time.deltaTime);
+
+        RaycastHit2D targetInRange = Physics2D.Raycast(
+            transform.position,
+            Vector2.left,
+            attackRange,
+            targetLayer
+        );
+
+        if (targetInRange) {
+            currentWalkSpeed = 0;
+            targetHealth = targetInRange.transform.GetComponent<Health>();
+            animator.SetBool("InAttackRange", true);
+        }
+        else {
+            animator.SetBool("InAttackRange", false);
+            targetHealth = null;
+        }
     }
 
     public void SetWalkSpeed(float speed) {
         currentWalkSpeed = speed;
+    }
+
+    public void Attack() {
+        if (targetHealth) {
+            targetHealth.TakeDamage(damage);
+        }
     }
 }
