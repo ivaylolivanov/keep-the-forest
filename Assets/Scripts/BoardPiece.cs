b0e29c731d03010b74b5101fb730a7479b182c2e
ivaylolivanov <ivaylolivanov95@gmail.com>
2021-03-06 17:08:12 +0200

    Combine attackers and defenders in a single script
    Signed-off-by: ivaylolivanov <ivaylolivanov95@gmail.com>

---
+++ Assets/Scripts/BoardPiece.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BoardPiece : MonoBehaviour {
+
+    public abstract void SetMovingDirectionToRunning();
+    public abstract void SetWalkSpeed(float speed);
+}
